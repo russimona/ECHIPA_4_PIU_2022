19dacf28392da4b46386d991bb1050c4edf119d4
russimona1200@gmail.com <russimona1200@gmail.com>
2022-12-02 12:20:06 +0200

    add login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,38 @@
 import React from "react";
 import { Button } from "reactstrap";
 
+//assets
+import eye from '../assets/violet-eye.png'
+//others
+import './login.css'
+
 const Login = () => {
   return (
     <div>
-      <h1>Login page</h1>
-      <Button>Login here</Button>
+      <div className="child-left">
+        <h2 style = {{justifyContent: 'center', display: 'flex', paddingTop: '30PX', marginBottom: '80px'}}>
+          Login to Your Account
+        </h2>
+        <label>
+          Email
+        </label>
+        <input placeholder="email"/>
+        <label>
+          Password
+        </label>
+        <input placeholder="password" type="password"/>
+        <Button>
+          LOGIN
+        </Button>
+      </div>
+
+      <div className="child-right">
+        <img src={eye}/>
+        <h1>
+          Eyes for everyone 
+        </h1>
+      </div>
+      
     </div>
   );
 };
